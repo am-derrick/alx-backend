f4f9a1753cba4d9c61b98657a9dc4606e4d6031f
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-29 16:14:07 +0300

    pep fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,5 +6,6 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ returns a tuple with corresponding index to range of indexes to return
-    in a list for particular pagination parameters."""
+        in a list for particular pagination parameters.
+    """
     return ((page - 1) * page_size, page * page_size)
