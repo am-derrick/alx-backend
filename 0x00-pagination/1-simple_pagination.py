6af1ffa06473b44d7c84eeafe1631973f5bc5aad
Derrick Ampire <65196859+am-derrick@users.noreply.github.com>
2022-04-29 16:14:58 +0300

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -27,3 +27,10 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
             pass
+
+        
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ returns a tuple with corresponding index to range of indexes to return
+        in a list for particular pagination parameters.
+    """
+    return ((page - 1) * page_size, page * page_size)
