55331f4fe60a4c5744a77dcfd264ebf0e0db24d3
Derrick Ampire <ampire90@gmail.com>
2022-04-29 14:09:18 +0000

    deletion-resilient

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,4 +40,23 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+            """deletion-resilient hypermedia pagination and returns
+            the index, data, page_size, next_index
+            """
+            assert(isinstance(index, int),
+                   isinstance(page_size, int),
+                   index, page_size) >= (True, True, 0, 1)
+            assert index < len(self.indexed_dataset())
+            data = []
+            next_index = index + page_size
+            for i in range(index, next_index):
+                while self.indexed_dataset().get(i) is None:
+                    i += 1
+                    next_index += 1
+                data.append(Self.indexed_dataset()[i])
+            return {
+                'index': index,
+                'data': data,
+                'page_size': page_size,
+                'next_index': next_index
+            }
