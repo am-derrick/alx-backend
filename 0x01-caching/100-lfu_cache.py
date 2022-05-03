672352afd7c3a982fbadedb90641c8e0976181ad
Derrick Ampire <ampire90@gmail.com>
2022-05-03 11:46:35 +0000

    caching algorithms

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""LFU Caching"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+def LFUCache(BaseCaching):
+    """LFU Caching System"""
+    def __init__(self):
+        """initialises the class instance"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+        self.mru = ""
+
+    def put(self, key, item):
+        """adds item in cache"""
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key in self.cache_data:
+                    self.cache_data.updat({key: item})
+                    self.mru
+                else:
+                    key_discarded = self.mru
+                    del self.cache_data[key_discarded]
+                    print('DISCARD: {}'.format(key_discarded))
+                    self.cache_data[key] = item
+                    self.mru = key
+            else:
+                self.cache_data[key] = item
+                self.mru = key
+
+    def get(self, key):
+        """gets na item corresponding to key"""
+        if key in self.cache_data:
+            self.mru = key
+            return self.cache_data[key]
---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""LRU Caching"""
+from base_caching import BaseCaching
+
+
+def LRUCache(BaseCaching):
+    """LRU Caching system"""
+    def __init__(self):
+        """initialises the calss instance"""
+        super().__init__()
+
+    def put(self, key, item):
+        """adds item value corresponding to key
+        the method move_to_end moves key to end to access lru
+        """
+        if key and item:
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            key_discarded = self.cache_data.popitem(last=False)
+            print('DISCARD: {}'.format(discarded[0]))
+
+    def get(self, key):
+        """gets an item corresponding to key"""
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
