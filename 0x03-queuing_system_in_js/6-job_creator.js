4274bb67d58c4b09c35336ec67d6f55880e7cc2c
Derrick Ampire <ampire90@gmail.com>
2024-06-26 14:11:26 +0300

    Update 6-job_creator.js: comma replaced instead of semi-colon

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,7 +2,7 @@ const kue = require('kue');
 
 const queue = kue.createQueue();
 const jobObj = {
-    phoneNumber: '4153518780';
+    phoneNumber: '4153518780',
     message: 'This is the code to verify your account',
 };
 
